74507b84844caa234b5e6f0bb67ed1eb2777c874
Kaili Nishihira <kailinishihira@gmail.com>
2017-07-20 21:28:53 -0700

    remove button will not work!

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,16 @@ function ToDo(task, due, priority) {
   this.priority = priority;
 }
 
-// ToDo.prototype.everythingList = function() {
-//   return this.task + this.due + this.priority;
-// }
+ToDo.prototype.toDoDetails = function() {
+  return this.task + " by " + this.due;
+}
 
+// ToDo.prototype.completed = function() {
+//   delete newToDo.task;
+//   delete newToDo.due;
+//   delete newToDo.priority;
+// }
+var newToDo;
 //UI logic
 $(document).ready(function() {
   $("form#new-to-do").submit(function(event) {
@@ -18,21 +24,25 @@ $(document).ready(function() {
     var inputtedDue = $("input#new-due-date").val();
     var inputtedPriority = $("select#new-priority").val();
 
-    var newToDo = new ToDo(inputtedTask, inputtedDue, inputtedPriority);
+    newToDo = new ToDo(inputtedTask, inputtedDue, inputtedPriority);
 
     $(".output").append("<li><span class='tasks'>" + newToDo.task + "</span></li>")
 
+    $(".tasks").last().click(function() {
+      $(".well").show();
+      $("#task").text(newToDo.toDoDetails());
+      $("#due").text(newToDo.due);
+      $("#priority").text(newToDo.priority);
+    });
 
-    $("#task").text(newToDo.task);
-    $("#due").text(newToDo.due);
-    $("#priority").text(newToDo.priority);
-    $(".well").show();
+      // $("#completed").click(function() {
+      //   debugger;
+      //   newToDo.hide();
+      // });
+
 
     $("input#new-task").val("");
     $("input#due-date").val("");
     $("select#priority").val("");
-
-
-
   });
 });
